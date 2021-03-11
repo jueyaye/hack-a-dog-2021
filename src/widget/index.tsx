a830da003deffffe7b695b1817c229542bd71729
jueyaye <daniel.ju@datadoghq.com>
2021-03-11 16:46:40 +1000

    add alt repo scrapping

--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -5,6 +5,8 @@ import { getRawAgentChangelog, parseRawAgentChangelog } from "../getAgentVersion
 
 import { getIntegrationChangelog, parseIntegrationChangelog, getIntegrationNames } from "../getIntegrationVersion";
 
+import { getRepos, filterRepos } from "../getAltRepos"
+
 function Widget() {
   const [latestAgentVersion, setLatestAgentVersion] = useState("");
   const [latestReleseDate, setLatestReleseDate] = useState("");
@@ -20,90 +22,28 @@ function Widget() {
       setLatestReleseDate(latestReleseDate);
     })
 
-    getIntegrationChangelog("airflow").then((res:JSON) => {
-      const { latestIntegrationVersion, integrationReleaseDate } = parseIntegrationChangelog(res);
-      console.log(latestIntegrationVersion);
-      console.log(integrationReleaseDate);
-      //console.log(res);
-      //setLatestAgentVersion(latestIntegrationVersion);
-      //setLatestReleseDate(latestReleseDate);
-    })
+    // getIntegrationChangelog("airflow").then((res:JSON) => {
+    //   const { latestIntegrationVersion, integrationReleaseDate } = parseIntegrationChangelog(res);
+    //   console.log(latestIntegrationVersion);
+    //   console.log(integrationReleaseDate);
+    //   //console.log(res);
+    //   //setLatestAgentVersion(latestIntegrationVersion);
+    //   //setLatestReleseDate(latestReleseDate);
+    // })
 
-    getIntegrationNames().then((res:JSON) => {
-      console.log(res);
-      //console.log(res);
-      //setLatestAgentVersion(latestIntegrationVersion);
-      //setLatestReleseDate(latestReleseDate);
+    // getIntegrationNames().then((res:JSON) => {
+    //   console.log(res);
+    //   //console.log(res);
+    //   //setLatestAgentVersion(latestIntegrationVersion);
+    //   //setLatestReleseDate(latestReleseDate);
+    // })
+
+    getRepos(1).then((res:unknown) => {
+      filterRepos(res).then((repos:unknown) => {
+      })
     })
     
   }, []);
-
-  const onOpenSidePanel = (args:any) => {  
-    var xhr = new XMLHttpRequest()
-    var completeResult = "";
-    var a = 0;
-    
-    var listOfIntegrations = ["airflow", "activemq", "apache"];
-    var baseUrl = "https://raw.githubusercontent.com/DataDog/integrations-core/master/";
-    var completeUrl = "";
-    for (var i = 0; i < listOfIntegrations.length; i++) {
-      completeUrl = baseUrl + listOfIntegrations[i] + "/CHANGELOG.md";
-      /*xhr.addEventListener('load', () => {
-        // update the state of the component with the result here
-        //console.log(xhr.responseText)
-        console.log("triggered " + a++)
-        completeResult = completeResult + xhr.responseText;
-        
-        
-        
-      })
-      xhr.open('GET', completeUrl)
-      // send the request
-      xhr.send()*/
-      setResultText("Result:")
-      fetch(completeUrl).then(res => {
-        console.log(res);
-        //setResult(res)
-      }).then((data) => {
-        console.log(data);
-          //
-      })
-
-      fetch(completeUrl).then(response => 
-        response.json().then(data => ({
-            data: data,
-            status: response.status
-        })
-      ).then(res => {
-          console.log(res.status, res.data.title)
-      }));
-
-
-      Promise.all([0, 1].map((index) => {
-        return new Promise((resolve, reject) => {
-          const xhr = new XMLHttpRequest()
-          xhr.open("GET", completeUrl, true)
-          xhr.onload = function() {
-            if (xhr.readyState === 4) {
-              if(xhr.status === 200) {
-                resolve(JSON.parse(xhr.responseText))
-              } else {
-                reject(xhr.statusText)
-              }
-          }
-        }
-        })
-      })).then((json_objs) => {
-        console.log(json_objs)
-      }).catch(err => console.error(err))
-    }
-    
-    
-    
-    
-  }
-
-
 
   return (
     <section style={{ padding: "10px" }}>
@@ -113,7 +53,6 @@ function Widget() {
 
       <h2>Integration Versions</h2>
       <p>Here is a list of the latest integration versions.</p>
-      <p><button className="button button-outline" onClick={onOpenSidePanel}>Show</button> </p>
       <h2>{resultText}</h2>
       <p>{result}</p>
     </section>
