6cd18ecbfb3570629084c507d763a0e1cc3e146a
Dalen Leung <dalen.leung@MacBook-Pro.local>
2021-03-11 17:29:03 +1100

    Add list of integrations with versions

--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -3,14 +3,15 @@ import { useEffect, useState } from "react";
 
 import { getRawAgentChangelog, parseRawAgentChangelog } from "../getAgentVersion";
 
-import { getIntegrationChangelog, parseIntegrationChangelog, getIntegrationNames } from "../getIntegrationVersion";
+import { getIntegrationChangelog, parseIntegrationChangelog, getIntegrationNames, parseIntegrationNames } from "../getIntegrationVersion";
 
 function Widget() {
   const [latestAgentVersion, setLatestAgentVersion] = useState("");
   const [latestReleseDate, setLatestReleseDate] = useState("");
 
   const [result, setResult] = useState("");
-  const [resultText, setResultText] = useState("");
+  const [resultHeading, setResultHeading] = useState("");
+  const [resultHeading2, setResultHeading2] = useState([""]);
 
   useEffect(() => {
     getRawAgentChangelog().then((res:JSON) => {
@@ -24,16 +25,24 @@ function Widget() {
       const { latestIntegrationVersion, integrationReleaseDate } = parseIntegrationChangelog(res);
       console.log(latestIntegrationVersion);
       console.log(integrationReleaseDate);
-      //console.log(res);
-      //setLatestAgentVersion(latestIntegrationVersion);
-      //setLatestReleseDate(latestReleseDate);
     })
 
     getIntegrationNames().then((res:JSON) => {
-      console.log(res);
-      //console.log(res);
-      //setLatestAgentVersion(latestIntegrationVersion);
-      //setLatestReleseDate(latestReleseDate);
+      const { listOfIntegrationNames, listOfIntegrationVersions } = parseIntegrationNames(res);
+      console.log('this is the list of integrations')
+      console.log(listOfIntegrationNames)
+      console.log("list of versions")
+      console.log(listOfIntegrationVersions)
+      var listOfNamesInString = "";
+      setResultHeading("Latest Integration Versions:")
+      
+      for (var i = 0; i < listOfIntegrationNames.length; i++) {
+        listOfNamesInString += listOfIntegrationNames[i] + " " + listOfIntegrationVersions[i] + "\n";
+      }
+      setResult(listOfNamesInString)
+      setResultHeading2(listOfIntegrationNames)
+      
+
     })
     
   }, []);
@@ -42,65 +51,8 @@ function Widget() {
     var xhr = new XMLHttpRequest()
     var completeResult = "";
     var a = 0;
-    
-    var listOfIntegrations = ["airflow", "activemq", "apache"];
-    var baseUrl = "https://raw.githubusercontent.com/DataDog/integrations-core/master/";
-    var completeUrl = "";
-    for (var i = 0; i < listOfIntegrations.length; i++) {
-      completeUrl = baseUrl + listOfIntegrations[i] + "/CHANGELOG.md";
-      /*xhr.addEventListener('load', () => {
-        // update the state of the component with the result here
-        //console.log(xhr.responseText)
-        console.log("triggered " + a++)
-        completeResult = completeResult + xhr.responseText;
-        
-        
-        
-      })
-      xhr.open('GET', completeUrl)
-      // send the request
-      xhr.send()*/
-      setResultText("Result:")
-      fetch(completeUrl).then(res => {
-        console.log(res);
-        //setResult(res)
-      }).then((data) => {
-        console.log(data);
-          //
-      })
-
-      fetch(completeUrl).then(response => 
-        response.json().then(data => ({
-            data: data,
-            status: response.status
-        })
-      ).then(res => {
-          console.log(res.status, res.data.title)
-      }));
 
 
-      Promise.all([0, 1].map((index) => {
-        return new Promise((resolve, reject) => {
-          const xhr = new XMLHttpRequest()
-          xhr.open("GET", completeUrl, true)
-          xhr.onload = function() {
-            if (xhr.readyState === 4) {
-              if(xhr.status === 200) {
-                resolve(JSON.parse(xhr.responseText))
-              } else {
-                reject(xhr.statusText)
-              }
-          }
-        }
-        })
-      })).then((json_objs) => {
-        console.log(json_objs)
-      }).catch(err => console.error(err))
-    }
-    
-    
-    
-    
   }
 
 
@@ -110,12 +62,14 @@ function Widget() {
       <h2>Agent Versions</h2>
       <p>Latest Datadog Agent version: {latestAgentVersion}</p>
       <p> Released on: {latestReleseDate}</p>
-
       <h2>Integration Versions</h2>
       <p>Here is a list of the latest integration versions.</p>
       <p><button className="button button-outline" onClick={onOpenSidePanel}>Show</button> </p>
-      <h2>{resultText}</h2>
-      <p>{result}</p>
+      <h2>{resultHeading}</h2>
+     
+      {resultHeading2.map(item => (
+            <li key={item}>{item}</li>
+          ))}
     </section>
   );
 }
