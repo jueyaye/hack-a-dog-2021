567edee53b616bd5871ff168086019c495b01b09
jueyaye <daniel.ju@datadoghq.com>
2021-03-12 12:55:57 +1000

    componentised versionCard

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Controller from "./controller";
-import Modal from "./modal";
-import SidePanel from "./side-panel";
 import Widget from "./widget";
 
 const getContent = () => {
   console.log(window.location.pathname);
   switch (window.location.pathname) {
-    case "/widget": {
-      return <Widget />;
-    }
-    case "/modal": {
-      return <Modal />;
-    }
-    case "/panel": {
-      return <SidePanel />;
-    }
     default: {
       return <Widget />;
       //Controller
--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -1,46 +1,18 @@
-
 import { useEffect, useState } from "react";
 
+// helpers
 import { getRawAgentChangelog, parseRawAgentChangelog } from "../getAgentVersion";
-
 import { getIntegrationNames, parseIntegrationNames } from "../getIntegrationVersion";
-
 import { getRepos, filterRepos } from "../getAltRepos"
 
-//Material UI Framework for Card
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    display: 'inline-block',
-    margin: 10
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+// components
+import VersionCard from "../versionCard"
 
 function Widget() {
   const [latestAgentVersion, setLatestAgentVersion] = useState("");
   const [latestReleseDate, setLatestReleseDate] = useState("");
 
   const [result, setResult] = useState([] as {name:string, version:string}[]);
-
-  const classes = useStyles();
 
   useEffect(() => {
     getRawAgentChangelog().then((res:JSON) => {
@@ -57,13 +29,13 @@ function Widget() {
 
     })
 
-    getRepos(1).then((res:any) => {
-      console.log(res)
+    // getRepos(1).then((res:any) => {
+    //   console.log(res)
 
-      filterRepos(res).then((repos:any) => {
-        console.log(repos)
-      })
-    })
+    //   filterRepos(res).then((repos:any) => {
+    //     console.log(repos)
+    //   })
+    // })
 
   
   }, []);
@@ -76,24 +48,10 @@ function Widget() {
       <h2>Integration Versions</h2>
       <p>Here is a list of the latest integration versions.</p>
      
-      {result.map((item, i) => (
-            <Card className={classes.root}>
-              <CardContent>
-                <Typography className="none" color="textSecondary" gutterBottom>
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  {item.name}
-                </Typography>
-                <Typography className="none" color="textSecondary">
-                  {item.version}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  Integration description
-                </Typography>
-              </CardContent>
-            </Card>
-          ))
-      }
+      { result.map((item, i) => (
+        <VersionCard item={item}/>
+      )) }
+
     </section>
     
   );
