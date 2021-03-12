f8a24a3165bf2d0fe1b45eddbfdb2a7558b50dc8
jueyaye <daniel.ju@datadoghq.com>
2021-03-12 12:58:11 +1000

    add missing file

---
+++ src/versionCard/index.tsx
@@ -0,0 +1,50 @@
+//Material UI Framework for Card
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    display: 'inline-block',
+    margin: 10
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function VersionCard(props:any) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className="none" color="textSecondary" gutterBottom>
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {props.item.name}
+        </Typography>
+        <Typography className="none" color="textSecondary">
+          {props.item.version}
+        </Typography>
+        <Typography variant="body2" component="p">
+          Integration description
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default VersionCard
